010e4fa2af335af9c8ce8b3d022f363a89504455
AHMMRK <ahmmrk.3570@gmail.com>
2025-12-25 09:34:00 +0330

    make addressing correct

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 
 const searchList = async (value) => {
-  const res = await fetch("./states.json");
+  const res = await fetch("./src/states.json");
   const states = await res.json();
 
   let matchesStates = states.filter((state) => {
